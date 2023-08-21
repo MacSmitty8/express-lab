f6a892d2b086464e1af52d82bbeb9b8838c80ad3
MacreenSmith <macreensmith@gmail.com>
2023-08-21 13:39:41 -0400

    Added New.jsx, and added the middleware and New index route.

--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const vegetables = require("./models/vegetables.js")
 app.set('views',__dirname + '/views');
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-
-
+//Middleware
+app.use((req, res, next) =>{
+    console.log("I run for all routes!")
+    next();
+})
+//this allows the body of a post request
+app.use(express.urlencoded({extended: false}))
 //ROUTES
 //index
 app.get('/fruits', (req, res) => {
@@ -18,7 +23,23 @@ app.get('/fruits', (req, res) => {
         fruits: fruits
     })
 })
+//New 
+app.get('/fruits/new', (req, res) => {
+    res.render('fruits/New')
+})
 
+//Create = Post
+app.post('/fruits', (req, res) =>{
+   console.log(req.body)
+   if(req.body.readyToEat === 'on'){
+    req.body.readyToEat = true;
+   } else {
+    req.body.readyToEat = false;
+   }
+   fruits.push(req.body)
+   console.log()
+    res.redirect('/fruits')
+})
 app.get('/fruits/:index', (req, res) =>{
     res.render('fruits/Show',{
         fruit: fruits[req.params.index]
